f160ddfa73b7361580d529715906de29d72721a8
Zoltan Nagy <abesto@abesto.net>
2015-12-12 23:18:44 +0100

    Solved puzzle 12

---
+++ 12/two.py
@@ -0,0 +1,26 @@
+import itertools
+import json
+
+
+def value_iterator(o):
+    if isinstance(o, dict):
+        if 'red' in o.itervalues():
+            return []
+        return itertools.chain(*[value_iterator(item) for item in o.itervalues()])
+    if isinstance(o, list):
+        return itertools.chain(*[value_iterator(item) for item in o])
+    return [o]
+
+
+def reduce_values(o):
+    val = 0
+    for item in value_iterator(o):
+        if isinstance(item, int):
+            val += item
+    return val
+
+
+if __name__ == '__main__':
+    with open('input', 'r') as f:
+        data = json.load(f)
+    print reduce_values(data)
